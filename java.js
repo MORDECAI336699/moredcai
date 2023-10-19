e5a9dcd5042a72815b897f5cb97f9c86af6ec8c0
Mordecai <adelekemordecai2019@gmail.com>
2023-10-19 03:11:29 -0700

    updated with the new file

---
+++ java.js
@@ -0,0 +1,13 @@
+console.log("i love pizza");
+console.log("because pizza is good");
+
+window.alert("mordecai site!")
+
+let age = 21;
+let firstname="bro"
+age = age + 6;
+
+console.log("you are",age, "years old");
+console.log("hello",firstname);
+
+document.getElementById("to1").innerHTML = "hello " + firstname;
